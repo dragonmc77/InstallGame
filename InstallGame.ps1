90c7c241ffe810053134a441c12ebb7eb975c3e5
dragonmc77 <dragonmc77@gmail.com>
2019-09-17 22:09:56 -0400

    Added handling of empty ISO Path. Added README.

--- InstallGame.ps1
+++ InstallGame.ps1
@@ -1,23 +1,27 @@
 function global:installGame() {
-    <#  #>
     $selection = $PlayniteApi.MainView.SelectedGames
+    <#  check that only one game is selected #>
     if ($selection.Count -gt 1) {
-        #TODO: display warning to select only one game and exit
         $PlayniteApi.Dialogs.ShowErrorMessage("Only one game can be installed at a time.","Too Many Games Selected")
         break
     }
     $selectedGame = $selection[0]
     $gameImagePath = $selectedGame.GameImagePath
-
+    <#  check if the ISO Path box is empty. if it is, provide the user with the opportunity to enter the
+        location of the installation media to install the game. 
+        Also check if the existing ISO Path for the game is actually valid.    
+    #>
     if ($null -eq $gameImagePath) {
-        #TODO: Prompt user for path to installation source
-        $PlayniteApi.Dialogs.ShowErrorMessage("The installation path is empty.","No Installation Path")
-        break
+        $response = $PlayniteApi.Dialogs.ShowMessage("The installation path is empty.`nDo you want to specify the location of the installation media?","No Installation Path",[System.Windows.MessageBoxButton]::YesNo)
+        if ($response -eq [System.Windows.MessageBoxResult]::Yes) {$gameImagePath = $PlayniteApi.Dialogs.SelectFolder()}
+        if ($null -eq $gameImagePath) {break}
     } elseif (-not (Test-Path $gameImagePath)) {
         $PlayniteApi.Dialogs.ShowErrorMessage("The file/folder specified in the installation path does not exist.","Invalid Path")
         break
     } 
-
+    <#  check if the ISO Path is a disk image file (.iso). if so, mount the .iso to a drive letter 
+        and run the installer (setup.exe) located there. then dismount the image
+    #>
     if (Test-Path -Path $gameImagePath -PathType Leaf -Include "*.iso") {
         $mountedDisk = Mount-DiskImage -ImagePath $gameImagePath
         $driveLetter = ($mountedDisk | Get-Volume).DriveLetter
@@ -26,12 +30,15 @@ function global:installGame() {
         global:runInstaller -Game $selectedGame -Path $installSource -FindSetup
         Dismount-DiskImage -ImagePath $mountedDisk.ImagePath
     }
+    <#  check if the ISO Path is an executable file (.exe). if so, run the executable to install the game #>
     elseif (Test-Path -Path $gameImagePath -PathType Leaf -Include "*.exe") {
         global:runInstaller -Game $selectedGame -Path $gameImagePath
     }
+    <#  check if the ISO Path is a folder location. if so, run the installer (setup.exe) located there #>
     elseif (Test-Path -Path $gameImagePath -PathType Container) {
         global:runInstaller -Game $selectedGame -Path $gameImagePath -FindSetup
     } 
+    <#  if all other checks failed, display an error #>
     else {
         $PlayniteApi.Dialogs.ShowErrorMessage("The file in the installation path is not recognized.","Unrecognized file")
     }
@@ -65,7 +72,7 @@ function global:runInstaller() {
             }
         }
     } else {
-        $PlayniteApi.Dialogs.ShowErrorMessage("Setup.exe was not found in the isntallation path.","Setup Not Found")
+        $PlayniteApi.Dialogs.ShowErrorMessage("Setup.exe was not found in the installation path.","Setup Not Found")
     }
 }
 
---
+++ README.md
@@ -0,0 +1,22 @@
+# InstallGame
+InstallGame Extension for Playnite
+
+Install as any extension:
+1. Create a folder called InstallGame in %appdata%\Playnite\Extensions.
+2. Copy the two files (InstallGame.ps1 and extension.yaml) to the folder.
+3. Make sure the extension is enabled in Playnite -> Settings -> Extensions.
+
+To use, select a game and run the appropriate command from the Extensions menu.
+
+History:
+I have my entire game library on Playnite, not just my currently installed games. Because my library consists of over 1000 games, most of the games in the library are not installed.
+Because I don't want to damage my game disks, I have converted all my games to ISO files and keep them in some location on a server. For each game, I put the path to its .ISO image in the "Image, ROM, or ISO Path" box of the Installation tab of that game in Playnite.
+Traditionally when I wanted to install a game, I would go into the game details, copy the path to the .ISO, mount it as a drive letter in Windows, then run the setup executable to install the game. I would then edit the game details with the appropriate paths for the Play Action and other relevant data.
+Because I found this cumbersome, I wrote this extension, which does all of the above automatically. 
+
+NOTES:
+Select a game, then click install from the Extensions menu. The extension will then mount the .ISO specified in the ISO Path and run the setup executable automatically. After the game is installed, it will prompt for the location of the game executable. Once specified, it will add the Play Action for that game, update the Installation Directory path, and mark it as installed.
+
+REQUIREMENTS:
+Because the extension calls an executable file, Playnite must be run with elevated privileges (Run as Administrator).
+This extension was thoroughly tested on Windows 10, but no testing was done on Windows 7. Because there may be differences in PowerShell versions on Windows 7, it unlikely this extension will work on that OS. Specifically, the method used to mount disk images may only work on Win10. Win7 compatibility may be something I address in the future.
